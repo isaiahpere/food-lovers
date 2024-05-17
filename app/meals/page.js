f2b624b8bdc04e4c3fe64347d70628df6fadb47f
Heber Pereira <isaiahpereira88@gmail.com>
2024-05-17 00:03:01 -0700

    meals grid and meal-item pages

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,8 +1,23 @@
+import Link from "next/link";
+import styles from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+
 const MealsPage = () => {
   return (
-    <div>
-      <h1>Meals</h1>
-    </div>
+    <>
+      <header className={styles.header}>
+        <h1>
+          Yummy meals, created <span className={styles.highlight}>by you</span>
+        </h1>
+        <p>Pick your fav recipe and try out! </p>
+        <p className={styles.cta}>
+          <Link href={"/meals/share"}>Share Fav Meal</Link>
+        </p>
+      </header>
+      <main className={styles.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
   );
 };
 
