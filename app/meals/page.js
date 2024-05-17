be0f665c3e89642a7d59f1bd56412d71f34aa840
Heber Pereira <isaiahpereira88@gmail.com>
2024-05-17 15:13:57 -0700

    error and loading pages

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,11 +1,18 @@
+import { Suspense } from "react";
 import Link from "next/link";
-import styles from "./page.module.css";
-import MealsGrid from "@/components/meals/meals-grid";
+
 import { getMeals } from "@/lib/meals";
+import MealsGrid from "@/components/meals/meals-grid";
 
-const MealsPage = async () => {
+import Loading from "./loading-out";
+import styles from "./page.module.css";
+
+const Meals = async () => {
   const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+};
 
+const MealsPage = () => {
   return (
     <>
       <header className={styles.header}>
@@ -18,7 +25,9 @@ const MealsPage = async () => {
         </p>
       </header>
       <main className={styles.main}>
-        <MealsGrid meals={meals} />
+        <Suspense fallback={<Loading />}>
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
